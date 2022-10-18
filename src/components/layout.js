8d1e864ce1e14bd1ae68ca1a34251d0e1be53bcb
Liam <liam.s.smith@gmail.com>
2022-10-17 23:07:49 -0400

    create layout component

---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+export default function Layout({pageTitle, children}){
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
