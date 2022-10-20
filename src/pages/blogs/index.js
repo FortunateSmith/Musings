aea7a4ef7b02cdac986f4469d737a723fed02c54
Liam <liam.s.smith@gmail.com>
2022-10-19 23:41:33 -0400

    finish module 6, update css and dynamic content for blog posts

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,35 +1,42 @@
 import * as React from "react";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
+import {navLinkItem, navLinkText} from "../../components/layout.module.css"
 
 export default function BlogPage({ data }) {
   return (
     <Layout pageTitle="Blog Posts">
- 
-        {data.allMdx.nodes.map((node) => {
-          return <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
-          <h5>Posted: {node.frontmatter.date}</h5>
-          <p>{node.excerpt}</p>
+      {data.allMdx.nodes.map((node) => {
+        return (
+          <article key={node.id}>
+            <h2 className={navLinkItem}>
+              <Link to={`/blogs/${node.frontmatter.slug}`}>
+                <span className={navLinkText}>{node.frontmatter.title}</span>
+              </Link>
+            </h2>
+            <h5>Posted: {node.frontmatter.date}</h5>
+            <p>{node.excerpt}</p>
           </article>
-        })}
-   
+        );
+      })}
     </Layout>
   );
 }
 
 export const query = graphql`
-query {
-  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-    nodes {
-      frontmatter {
-        date(formatString: "MMMM D, YYYY")
-        title
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
+        excerpt
       }
-      id
-      excerpt
     }
   }
-}`
+`;
 export const Head = () => <Seo title="Blog Posts" />;
--- src/pages/blogs/{mdx.frontmatter__slug}.js
+++ src/pages/blogs/{mdx.frontmatter__slug}.js
@@ -3,28 +3,25 @@ import * as React from "react";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
-export default function BlogPost({data}) {
+export default function BlogPost({ data, children }) {
   return (
-    <Layout pageTitle="My Blog Posts">
-      <p>My Blog Posts Will Go *Here* (Eventually)</p>
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
     </Layout>
   );
 }
 
-
 export const query = graphql`
-query ($id: String) {
-  mdx (id: {eq: $id}){
-    frontmatter{
-      title
-      date(formatString: "MMMM D, YYYY")
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
     }
   }
-}
 `;
-// console.log("frontmatter props")
+// console.log("frontmatter props") 
 
-
-
-export const Head = () => <Seo title="My Blog Posts"/>
-
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />;
